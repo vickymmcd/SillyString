b39bc4ed9462700c501581cb5156f659f9ca7d0b
Vicky McDermott <vickymmcd@gmail.com>
2018-02-26 20:40:54 -0500

    working on exercises

--- code/hello_world.c
+++ code/hello_world.c
@@ -1,32 +1,20 @@
-
-#include <ncurses.h>
+#include <ncurses.h>			/* ncurses.h includes stdio.h */
+#include <string.h>
 
 int main()
-{	int ch;
+{
+ char mesg[]="Just a string";		/* message to be appeared on the screen */
+ int row,col;				/* to store the number of rows and *
+					 * the number of colums of the screen */
+ initscr();				/* start the curses mode */
+ getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+ mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+                                	/* print the message at the center of the screen */
+ mvprintw(row-2,0,"This screen has %d rows and %d columns\n",row,col);
+ printw("Try resizing your window(if possible) and then run this program again");
+ refresh();
+ getch();
+ endwin();
 
-	initscr();			/* Start curses mode 		*/
-	raw();				/* Line buffering disabled	*/
-	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
-	noecho();			/* Don't echo() while we do getch */
-
-    	printw("Type any character to see it in bold\n");
-	ch = getch();			/* If raw() hadn't been called
-					 * we have to press enter before it
-					 * gets to the program 		*/
-	if(ch == KEY_F(1))		/* Without keypad enabled this will */
-		printw("F1 Key pressed");/*  not get to us either	*/
-					/* Without noecho() some ugly escape
-					 * charachters might have been printed
-					 * on screen			*/
-	else
-	{	printw("The pressed key is ");
-		attron(A_BOLD);
-		printw("%c", ch);
-		attroff(A_BOLD);
-	}
-	refresh();			/* Print it on to the real screen */
-    	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
-
-	return 0;
+ return 0;
 }
