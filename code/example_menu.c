543c9e93d09a93a66ffb7406a72ab56795c06357
Vicky McDermott <vickymmcd@gmail.com>
2018-03-01 18:52:03 -0500

    finishing up the example menu

--- code/example_menu.c
+++ code/example_menu.c
@@ -39,30 +39,36 @@ int main(int argc, char *argv[])
     }
 
     ITEM **my_item;
-    int n_choices = 3;
+    int n_choices = 5;
     int c;
     my_item = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
-    my_item[0] = new_item("hello", "hiya");
-    my_item[1] = new_item("goodbye", "byea");
+    my_item[0] = new_item("A", "insert choice a here");
+    my_item[1] = new_item("B", "insert choice b here");
+    my_item[2] = new_item("C", "insert choice c here");
+    my_item[3] = new_item("D", "insert choice d here");
     MENU *my_menu = new_menu(my_item);
-    mvprintw(LINES - 2, 0, "F6 to Exit");
     post_menu(my_menu);
     refresh();
 
-    while((c = getch()) != KEY_F(6))
+    while((c = getch()) != '\r')
     {   switch(c)
         { case KEY_DOWN:
-          menu_driver(my_menu, REQ_DOWN_ITEM);
-          break;
+            menu_driver(my_menu, REQ_DOWN_ITEM);
+            break;
           case KEY_UP:
-          menu_driver(my_menu, REQ_UP_ITEM);
-          break;
+            menu_driver(my_menu, REQ_UP_ITEM);
+            break;
         }
       }
-
+    clear();
+    printw("You chose choice: %s\n", item_name(current_item(my_menu)));
+    printw("Which is: %s", item_description(current_item(my_menu)));
     free_item(my_item[0]);
     free_item(my_item[1]);
+    free_item(my_item[2]);
+    free_item(my_item[3]);
     free_menu(my_menu);
+    getch();
 
     finish(0);               /* we're done */
 }
